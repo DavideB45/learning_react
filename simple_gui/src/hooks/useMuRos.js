71960b4f39fb0f0f127704939a0e8291c0d1ef93
Davide Borghini <dadewww@gmail.com>
2026-06-19 16:42:01 +0200

    mu ros hook

---
+++ simple_gui/src/hooks/useMuRos.js
@@ -0,0 +1,93 @@
+import { use, useEffect, useState } from "react"
+
+function setMuIP(address, boardIP){
+	const match = address.match(/^(\d{1,3}(?:\.\d{1,3}){3}):(\d{1,5})$/);
+	if (!match) {
+		throw new Error('Address must be in format: 192.168.1.10:9876');
+	}
+	const ip = match[1];
+	const port = match[2];
+	return fetch(`http://${boardIP}/microros`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				agent_ip: ip,
+				agent_port: port
+			})
+	})
+	.then(response => {
+		if (!response.ok) {
+			throw new Error("POST failed");
+		}
+		return response.json()
+	});
+}
+
+export function useMuRos(boardIP, ws, muRosIP) {
+  const [muRosStatus, setMuRosStatus] = useState('idle');
+
+	const connect = () => {
+		if (!muRosIP || !boardIP || !ws) return;
+		ws.onmessage = (event) => {
+			console.log("new message")
+			try {
+				const msg = JSON.parse(event.data);
+				console.log(msg.ws_data_type)
+				if(msg.ws_data_type != "system_status") return
+				if (msg.microros.connected) {
+					setMuRosStatus("ready");
+				} else {
+					setMuRosStatus("error");
+				}
+			} catch (e) {
+				console.error("Bad websocket message", e);
+			}
+		};
+		setMuRosStatus("loading");
+		setMuIP(muRosIP, boardIP)
+		.then(data => {
+			console.log(data);
+		})
+		.catch(error => {
+			console.error(error);
+			setMuRosStatus('error');
+		});
+	};
+
+	useEffect(() => {
+		if (!ws || !muRosIP) return;
+		console.log("setting handler")
+		ws.onmessage = (event) => {
+			console.log("new message")
+			try {
+				const msg = JSON.parse(event.data);
+				console.log(msg.ws_data_type)
+				if(msg.ws_data_type != "system_status") return
+				if (msg.microros.connected) {
+					setMuRosStatus("ready");
+				} else {
+					setMuRosStatus("error");
+				}
+			} catch (e) {
+				console.error("Bad websocket message", e);
+			}
+		};
+		return () => {
+			ws.onmessage = () => {}
+		};
+	}, [ws, muRosIP]);
+  
+	useEffect( () => {
+		console.log(muRosIP)
+		console.log(!ws)
+		if (!muRosIP || !boardIP || !ws) {
+		setMuRosStatus('idle');
+		return;
+		}
+		connect()
+	}, [muRosIP, boardIP, ws]);
+
+  return { muRosStatus, retryMuRos: connect }
+}
